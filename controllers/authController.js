65024a8916fa0f4ea0de8f37a92dc132c800d97d
nowalor <no@walor.io>
2023-07-09 22:44:39 +0200

    feat: start implement register

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,7 +1,23 @@
 const db = require('../database/db')
 const jwt = require('jsonwebtoken')
+const bycrpt = require('bcrypt')
 
 const tempKeyReplaceLater = '12345'
+
+async function register(req, res) {
+    const { email, password } = req.body
+
+    const user = await db.select('*').from('users').where('email', email).first()
+
+    if(user) {
+        return res.status(422).json({
+            message: 'User with this email already exists'
+        })
+    }
+
+    const hashedPassword = bycrpt.hashSync(password, 10)
+    res.send(hashedPassword)
+}
 
 async function login(req, res) {
     const { email, password } = req.body
